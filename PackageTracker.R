ff74cfa0d1b45700c11077901123ace6ae4aa10e
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2021-07-16 17:05:29 +0100

    Update PackageTracker.R

--- PackageTracker.R
+++ PackageTracker.R
@@ -1,18 +1,21 @@
 #Create download log of NHSDataDictionaRy package
 library(ggplot2)
 library(dlstats)
+library(tibble)
 
-data_dict_status <- dlstats::cran_stats(c("caret", "parsnip", "reticulate", "keras", "NHSDataDictionaRy"))
+data_dict_status <- dlstats::cran_stats(c("NHSDataDictionaRy", "NHSRdatasets", "OddsPlotty",
+                                          "FeatureTerminatoR"))
 
 if (!is.null(data_dict_status)){
   head(data_dict_status)
   plot <- ggplot(data_dict_status,
                  aes(end, downloads, group=package)) + geom_line(aes(color=package),linetype="dashed") +
-          geom_point(aes(shape=package, color=package)) + theme_minimal()
-
+    geom_point(aes(shape=package, color=package)) + theme_minimal()
+  
   plot <- plot + xlab("Download date") + ylab("Number of downloads")
   print(plot)
 }
+print(plot)
 
 
 package_list <- list("NHSDataDictionaRy_plot"= plot,
@@ -20,5 +23,4 @@ package_list <- list("NHSDataDictionaRy_plot"= plot,
                      "downloads_to_date"=sum(data_dict_status$downloads))
 
 
-package_list$download_df
-
+df <- package_list$download_df
