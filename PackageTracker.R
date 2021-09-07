2d85334583ea86f668da4ac7017d0d75f480a0e6
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2021-09-07 17:01:30 +0100

    Update PackageTracker.R

--- PackageTracker.R
+++ PackageTracker.R
@@ -1,26 +1,29 @@
-#Create download log of NHSDataDictionaRy package
+#Load these libraries
 library(ggplot2)
 library(dlstats)
 library(tibble)
 
-data_dict_status <- dlstats::cran_stats(c("NHSDataDictionaRy", "NHSRdatasets", "OddsPlotty",
-                                          "FeatureTerminatoR"))
-
-if (!is.null(data_dict_status)){
-  head(data_dict_status)
-  plot <- ggplot(data_dict_status,
-                 aes(end, downloads, group=package)) + geom_line(aes(color=package),linetype="dashed") +
-    geom_point(aes(shape=package, color=package)) + theme_minimal()
-  
-  plot <- plot + xlab("Download date") + ylab("Number of downloads")
-  print(plot)
+#Create the wrapper function
+package_trackeR <- function(packages){
+    #Create the downloads for the package
+    dl <- dlstats::cran_stats(c(packages))
+    #Create the plot
+    plot <- ggplot(dl,
+                aes(end, downloads, group=package)) + 
+                geom_line(aes(color=package),linetype="dashed") +
+                geom_point(aes(shape=package, color=package)) + 
+                theme_minimal()
+    plot <- plot + xlab("Download date") + 
+            ylab("Number of downloads")
+    #Create a list for multiple returns
+    returns_list <- list("download_df"=as_tibble(dl),
+                        "downloads_to_date"=sum(dl$downloads),
+                        "downloads_plot"=plot)
+    
+    return(returns_list)
 }
-print(plot)
+
+#Call the new function
+package_trackeR(c("NHSDataDictionaRy"))
 
 
-package_list <- list("NHSDataDictionaRy_plot"= plot,
-                     "download_df"=as_tibble(data_dict_status),
-                     "downloads_to_date"=sum(data_dict_status$downloads))
-
-
-df <- package_list$download_df
